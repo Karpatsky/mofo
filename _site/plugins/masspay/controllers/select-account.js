8b3bdf75e72712c8eb4f89f736106504aa02240b
Dirk <incentivetoken@gmail.com>
2014-12-12 17:30:09 +0100

    Update mofo

--- _site/plugins/masspay/controllers/select-account.js
+++ _site/plugins/masspay/controllers/select-account.js
@@ -4,8 +4,8 @@ var module = angular.module('fim.base');
 module.controller('MassPayPluginAccountModalController', function(items, $modalInstance, $scope) {
   $scope.items = items;
   
-  $scope.items.accountRS = 'FIM-5PGB-BFNZ-KCSF-9XJWB';
-  $scope.items.secretPhrase = 'embarrass thorn anywhere beside myself dawn sorrow hurl painful witch lesson torture';
+  $scope.items.accountRS = '';
+  $scope.items.secretPhrase = '';
 
   $scope.close = function () {
     $modalInstance.close($scope.items);
