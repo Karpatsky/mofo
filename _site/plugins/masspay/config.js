1f79e065642d2b46900487946a47aeb1d80a8dbb
Dirk <incentivetoken@gmail.com>
2014-12-12 17:38:39 +0100

    Update mofo

--- _site/plugins/masspay/config.js
+++ _site/plugins/masspay/config.js
@@ -9,10 +9,10 @@ module.run(function (plugins, modals, $q, $timeout) {
     id:       'masspay',
     label:    'Mass Payer',
     author:   'FIMKrypto',
-    version:  '0.1',
+    version:  '0.2',
     extends:  'plugins',
     icon_class:   'glyphicon glyphicon-th',
-    templateURL:  'plugins/masspay/partials/masspay2.html',    
+    templateURL:  'plugins/masspay/partials/masspay.html',    
     createOnWalletFileSelectedPromise: function () {
       gDeferred = $q.defer();
       return gDeferred.promise;
