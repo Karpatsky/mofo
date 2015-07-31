3288e231246f88a54bdec2c3e7bc1aad8618cee6
Dirk <incentivetoken@gmail.com>
2015-07-31 15:18:49 +0200

    Update mofo

--- _site/mode.js
+++ _site/mode.js
@@ -4,6 +4,6 @@ var ENABLE_DUAL_ENGINES=true;
 var IS_TEST_NET=false;
 var FORCE_LOCAL_HOST=false;
 var PRIVATE_ENABLED=false;
-var WALLET_NAME='Mofo';
+var WALLET_NAME='Lompsa.com';
 var TRADE_UI_ONLY=false;
 var DEBUG=false;
